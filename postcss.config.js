14592f2fe3d63d8caa567685a673fb7d68803611
puneeth-m-s <pes2ug23cs817@pesu.pes.edu>
2025-08-19 18:13:56 +0530

    19/08

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},   // ✅ instead of require("tailwindcss")
+    autoprefixer: {}
+  },
+};
+
---
+++ src/.storybook/main.js
@@ -0,0 +1,19 @@
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  }
+};
+export default config;
